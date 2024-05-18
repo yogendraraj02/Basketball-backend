811fd58a8c1e3c6f992c6f8e18c9ac46bf27d774
Yogendra Raj <yogendraraj0206@gmail.com>
2024-05-18 21:45:18 +0530

    completed c-11 db connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user.routes");
+const { Sequelize } = require("sequelize");
 
 app.use(express.json())
 app.use('/users',userRoutes);
@@ -8,6 +11,12 @@ app.get('/' , (req, res) => {
     res.send("Basketball backend");
 })
 
-app.listen(3000 , () => {
-    console.log(`server listening at 3000`);
+app.listen(3000 , async () => {
+    try{
+        const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+        await sequelize.authenticate()
+        console.log(`server listening at 3000`);
+    } catch(e){
+        console.log(`error`,e);
+    }
 })
